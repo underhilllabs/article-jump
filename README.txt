c1b608a763060e5e9049d8d3e3e4d3f435ee66e1
bartlantz <bartlantz@9591.no-reply.drupal.org>
2012-07-15 01:02:42 -0400

    initial commit

---
+++ README.txt
@@ -0,0 +1,4 @@
+Article Jump
+==============
+
+Drupal module to add jQuery function to jump from one article to the next via a hotkey.
---
+++ artjump.js
@@ -0,0 +1,36 @@
+// define an array of articles and the current article index
+jQuery(function() {
+    my_select = Drupal.settings.artjump.my_selector || ".content article";
+    my_select = Drupal.settings.artjump.my_selector;
+    arts = jQuery(".content article");
+    idx = 0;
+
+});
+
+// Bind "n" to scroll window to the next article header
+Mousetrap.bind('n', function() {
+    idx++;
+    if(idx == arts.length) {
+        idx=0;
+        // or if h
+        // or idx = idx-1
+    }
+    jQuery('html,body').animate({
+        scrollTop:(jQuery(arts[idx]).offset().top - 100)
+    }, 500);
+});
+// Bind "p" to scroll window to the previous article header
+Mousetrap.bind('p', function() {
+    idx--;
+    if(idx < 0) {
+        // loop around to end??
+        //idx=arts.length-1;
+        idx=0;
+    }
+    jQuery('html,body').animate({
+        scrollTop:(jQuery(arts[idx]).offset().top - 100)
+    }, 500);
+});
+
+
+
