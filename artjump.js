7660ce5d782caa7e7e320f61da2281002f15ae0b
bartlantz <bartlantz@9591.no-reply.drupal.org>
2012-07-15 19:46:08 -0400

    Updated artjump.js to use Drupal behaviors.

--- artjump.js
+++ artjump.js
@@ -1,30 +1,30 @@
-(function() {
-  // define an array of articles and the current article index
-  jQuery(function() {
-      my_select = Drupal.settings.artjump.my_selector || ".content article, .content node-teaser";
-      my_speed = Drupal.settings.artjump.scroll_speed || '500';
+Drupal.behaviors.artjump = {
+    attach: function(context, settings) {
+      my_select = Drupal.settings.artjump.my_selector;
+      my_speed = Drupal.settings.artjump.scroll_speed;
       arts = jQuery(my_select);
-      idx = 0;
-  });
+      idx = -1;
 
-  // Bind "n" to scroll window to the next article header
-  Mousetrap.bind("n", function() {
-      idx++;
-      if(idx == arts.length) {
-          idx=0;
-      }
-      jQuery('html,body').animate({
-          scrollTop:(jQuery(arts[idx]).offset().top - 100)
-      }, my_speed);
-  });
-  // Bind "p" to scroll window to the previous article header
-  Mousetrap.bind("p", function() {
-      idx--;
-      if(idx < 0) {
-          idx=0;
-      }
-      jQuery('html,body').animate({
-          scrollTop:(jQuery(arts[idx]).offset().top - 100)
-      }, my_speed);
-  });
-}());
+      // Bind "n" to scroll window to the next article header
+      Mousetrap.bind("n", function() {
+          idx++;
+          if(idx == arts.length) {
+              idx=0;
+          }
+          jQuery('html,body').animate({
+              scrollTop:(jQuery(arts[idx]).offset().top - 100)
+          }, my_speed);
+      });
+
+      // Bind "p" to scroll window to the previous article header
+      Mousetrap.bind("p", function() {
+          idx--;
+          if(idx < 0) {
+              idx=0;
+          }
+          jQuery('html,body').animate({
+              scrollTop:(jQuery(arts[idx]).offset().top - 100)
+          }, my_speed);
+      });
+    }
+};
