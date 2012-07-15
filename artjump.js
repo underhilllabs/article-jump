4c1cf5ecb1993ee7a087097376200caea79077ef
bartlantz <bartlantz@9591.no-reply.drupal.org>
2012-07-15 14:56:03 -0400

    Added scroll speed option. Fixed some drupal code standards

--- artjump.js
+++ artjump.js
@@ -1,35 +1,30 @@
 (function() {
-// define an array of articles and the current article index
-jQuery(function() {
-    my_select = Drupal.settings.artjump.my_selector || ".content article, .content node-teaser";
-    arts = jQuery(my_select);
-    idx = 0;
+  // define an array of articles and the current article index
+  jQuery(function() {
+      my_select = Drupal.settings.artjump.my_selector || ".content article, .content node-teaser";
+      my_speed = Drupal.settings.artjump.scroll_speed || '500';
+      arts = jQuery(my_select);
+      idx = 0;
+  });
 
-});
-
-// Bind "n" to scroll window to the next article header
-Mousetrap.bind('n', function() {
-    idx++;
-    if(idx == arts.length) {
-        //window.location=jQuery(".pager-next > a")[0].href;
-        idx=0;
-        // or if h
-        // or idx = idx-1
-    }
-    jQuery('html,body').animate({
-        scrollTop:(jQuery(arts[idx]).offset().top - 100)
-    }, 500);
-});
-// Bind "p" to scroll window to the previous article header
-Mousetrap.bind('p', function() {
-    idx--;
-    if(idx < 0) {
-        // loop around to end??
-        //idx=arts.length-1;
-        idx=0;
-    }
-    jQuery('html,body').animate({
-        scrollTop:(jQuery(arts[idx]).offset().top - 100)
-    }, 500);
-});
+  // Bind "n" to scroll window to the next article header
+  Mousetrap.bind("n", function() {
+      idx++;
+      if(idx == arts.length) {
+          idx=0;
+      }
+      jQuery('html,body').animate({
+          scrollTop:(jQuery(arts[idx]).offset().top - 100)
+      }, my_speed);
+  });
+  // Bind "p" to scroll window to the previous article header
+  Mousetrap.bind("p", function() {
+      idx--;
+      if(idx < 0) {
+          idx=0;
+      }
+      jQuery('html,body').animate({
+          scrollTop:(jQuery(arts[idx]).offset().top - 100)
+      }, my_speed);
+  });
 }());
